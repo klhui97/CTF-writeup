329a96afdc36a20cf4cddfe99aa8f1334fb27c82
KL <huikldavid@gmail.com>
2018-09-13 01:43:36 +0800

    update rsa

--- Cryptography/RSA(private key only)/rsaDecrypt.py
+++ Cryptography/RSA(private key only)/rsaDecrypt.py
@@ -1,10 +1,7 @@
 from Crypto.PublicKey import RSA
-from Crypto.Signature import PKCS1_v1_5
-from Crypto.Hash import SHA
 from base64 import b64decode
 
 rsa_key = RSA.importKey(open('private.txt', "rb").read())
-verifier = PKCS1_v1_5.new(rsa_key)
 raw_cipher_data = b64decode('R1ywgy8D14DFhZVImslTjsZ5Ixso3ljxJiG7luayGwbBH/vE94as4/LXJ3hOfXsMmAsUwB/vYQE5ScxGrkTRxw==')
 phn = rsa_key.decrypt(raw_cipher_data)
 print(phn)
